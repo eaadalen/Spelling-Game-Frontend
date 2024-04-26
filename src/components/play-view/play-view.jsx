50c9bfb334362deff605aa2ad742afc0d61357b0
eaadalen <eaadalen112@gmail.com>
2024-04-25 22:05:30 -0500

    Play sound updates

--- src/components/play-view/play-view.jsx
+++ src/components/play-view/play-view.jsx
@@ -1,47 +1,34 @@
-import { get } from "http";
+import { create } from "domain";
 import "./play-view.scss"
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Form from "react-bootstrap/Form";
 
 export const PlayView = () => {
     const [word, setWord] = useState("");
     const [spelling, setSpelling] = useState("");
-    const [soundID, setSoundID] = useState("");
 
-    const generateWord = () => {
-      fetch(
-        "https://random-word-api.herokuapp.com/word",
-      )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("word: " + String(data))
-        setWord(data);
-        getSoundID();
-      });
+    async function generateWord() {
+      const response = await fetch("https://random-word-api.herokuapp.com/word")
+      const response_json = await response.json()
+      return response_json
     }
 
-    const getSoundID = () => {
-      var dict_url = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + String(word) + "?key=aede8a6f-61af-4667-bd27-95b2786bca10";
-
-      fetch(dict_url)
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.length < 20 && data != undefined) {  // Check if randomly generated word is in Merriam Webster database
-          console.log("audio code: " + String(data[0]["hwi"]["prs"][0]["sound"]["audio"]))
-          setSoundID(String(data[0]["hwi"]["prs"][0]["sound"]["audio"]))
-          create_sound_URL();
-        }
-      });
+    async function getSoundID(random) {
+      var dict_url = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + random + "?key=aede8a6f-61af-4667-bd27-95b2786bca10";
+      const response = await fetch(dict_url)
+      const response_json = await response.json()
+      if (response_json.length < 20 && response_json != undefined) {  // Check if randomly generated word is in Merriam Webster database
+        return String(response_json[0]["hwi"]["prs"][0]["sound"]["audio"])
+      }
     }
 
-    const create_sound_URL = () => {
-      var sound_url = "https://media.merriam-webster.com/audio/prons/en/us/mp3/"  + word[0].charAt(0) + "/" + soundID + ".mp3";
-      console.log(sound_url)
+    async function create_sound_URL(raw_word, soundID) {
+      var sound_url = "https://media.merriam-webster.com/audio/prons/en/us/mp3/"  + raw_word[0].charAt(0) + "/" + soundID + ".mp3";
 
       fetch(sound_url)
       .then(res => res.blob())
       .then((myBlob) => {
-          console.log('here3')
+          console.log(sound_url)
           const objectURL = URL.createObjectURL(myBlob);
           const newAudioURL = objectURL;
           var a = new Audio(newAudioURL);
@@ -49,8 +36,15 @@ export const PlayView = () => {
       });
     }
 
-    const playSound = () => {
-      generateWord();
+    async function playSound() {
+      const random_word = await generateWord();
+      const calc_soundID = await getSoundID(random_word);
+      if (calc_soundID != undefined) {
+        create_sound_URL(random_word, calc_soundID);
+      }
+      console.log(random_word)
+      console.log(calc_soundID)
+      //create_sound_URL();
       //var a = new Audio(url);
       //a.play();
     };
